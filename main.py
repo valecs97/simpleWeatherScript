8ad032cbdf6daac5016d007c853567689279bbe5
Vitoc Alecs <nightfuryva@gmail.com>
2019-02-28 14:03:26 +0200

    Wheater script

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+import requests
+import json
+
+if __name__ == '__main__':
+    pass
+if len(sys.argv) == 1:
+    print('Not enough arguments')
+    exit(1)
+
+apikey = '2ac04471f9d1ce57945ab013f3522809'
+city = sys.argv[1]
+
+r = requests.post("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID="+apikey)
+response = json.loads(r.text)
+celsiusDegrees = response['main']['temp'] - 273.15
+sky = response['weather'][0]['main']
+print(celsiusDegrees)
+print(sky)
+
